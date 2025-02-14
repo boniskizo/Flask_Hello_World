57be74816d43014344dcfdb05e51be5a9c0e51ec
boniskizo <boniskizo@gmail.com>
2025-02-14 10:57:28 +0100

    Change contact route

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def exercices():
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template('contact.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
